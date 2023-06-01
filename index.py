5fffba3ba6b110e1d5a3dd30c7593552f4f7b44f
robertrb63 <81638977+robertrb63@users.noreply.github.com>
2023-06-01 15:30:40 -0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app= Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('name.html')
+
+@app.route('/lenguajes')
+def mostrarLenguajes():
+    misLenguajes=("PHP", "Python", "Java", "JavaScripot", "Perl", "Ruby", "Rust")
+    return render_template('lenguajes.html', lenguajes=misLenguajes)
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
